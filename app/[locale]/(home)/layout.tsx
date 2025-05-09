6b545757714477c275b27019e02863be0f94e09b
Ruhul <brainboostservices@gmail.com>
2025-05-09 20:38:19 +0600

    fix: correct class name in HomeLayout component for proper styling

--- app/[locale]/(home)/layout.tsx
+++ app/[locale]/(home)/layout.tsx
@@ -7,7 +7,7 @@ export default async function HomeLayout({
   children: React.ReactNode
 }) {
   return (
-    <div className='flex flex-col min-h-screen d'>
+    <div className='flex flex-col min-h-screen'>
       <Header />
       <main className='flex-1 flex flex-col'>{children}</main>
       <Footer />
